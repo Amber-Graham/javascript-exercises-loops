8fcd5698354e390e3f32c91e2e8a1a9fb2215c75
Amber-Graham <ang190045@gmail.com>
2022-07-14 11:29:42 -0500

    complete 4 tests all pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
+}
 
-};
+reverseString("Hello");
+reverseString("Hello there");
+reverseString("123! abc!");
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
