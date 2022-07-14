791295b8babdc5fae1b05bdcb7d5a0dbb82dff42
Amber-Graham <ang190045@gmail.com>
2022-07-14 12:24:01 -0500

    complete all exercises and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,14 @@
-const removeFromArray = function(array1, ...arg) {
-    if (arg == 3) {
-        array1.splice(2,1);
-        return array1;
-    }
-}
+const removeFromArray = function(array1, ...args) {
+    return array1.filter(x => !args.includes(x))
+};
 
 removeFromArray([1, 2, 3, 4], 3);
-
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
