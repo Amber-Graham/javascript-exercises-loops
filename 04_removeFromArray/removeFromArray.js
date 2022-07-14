44b6875ee95422a32bdc2a1823794c064c8352c6
Amber-Graham <ang190045@gmail.com>
2022-07-14 12:09:17 -0500

    complete first test and write out function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...arg) {
+    if (arg == 3) {
+        array1.splice(2,1);
+        return array1;
+    }
+}
 
-};
+removeFromArray([1, 2, 3, 4], 3);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
