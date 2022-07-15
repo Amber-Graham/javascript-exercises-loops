3db05195b5b9f5673d31cdca43c10a2ea67642b2
Amber-Graham <ang190045@gmail.com>
2022-07-15 10:08:41 -0500

    complete all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,18 @@
-const sumAll = function(num1, num2) {
-    if(num1 < num2) {
-        let sum = num2 * (num2 + 1)/2 - num1 * (num1 + 1)/2 + num1;
+//user will give a range of numbers (num1 and num2)
+// numbers will be input into a formula to find the sum of all the integers in the range
+// if a bigger number is entered in the num1 slot, reverse the formula so negatives don't become an issue
+// once the formula is done, return the sum
+
+const sumAll = function(int1, int2) {
+    if(!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";    
+    if(int1 == '' || int2 == '' ||
+       int1 < 0 || int2 < 0) {
+       return 'ERROR';
+    } else if(int1 < int2) {
+        let sum = int2 * (int2 + 1)/2 - int1 * (int1 + 1)/2 + int1;
         return sum;
-    } else if(num1 > num2){
-        let sum = num1 * (num1 + 1)/2 - num2 * (num2 + 1)/2 + num2;
+    } else if(int1 > int2){
+        let sum = int1 * (int1 + 1)/2 - int2 * (int2 + 1)/2 + int2;
         return sum;
     } else {
         return 'ERROR';
@@ -13,6 +22,9 @@ const sumAll = function(num1, num2) {
 sumAll(1, 4);
 sumAll(1, 4000);
 sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(10, "90");
+sumAll(10 [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
