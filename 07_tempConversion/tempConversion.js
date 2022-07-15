c22feeeba49cb1edb286de5a3d6424a396097f09
Amber-Graham <ang190045@gmail.com>
2022-07-15 15:57:05 -0500

    test conditions and execute code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(numF) {
+  let celsius = (numF - 32) * 5/9;
+  let celsiusFinal = parseFloat(celsius.toFixed(1));
+  return celsiusFinal;
 };
 
-const ctof = function() {
-
+const ctof = function(numC) {
+  let fahrenheit = (numC * 1.8) + 32;
+  let fahrenheitFinal = parseFloat(fahrenheit.toFixed(1));
+  return fahrenheitFinal;
 };
+
+ftoc(32);
+ftoc(100);
+ftoc(-100);
+ctof(0);
+ctof(73.2);
+ctof(-10);
 
 // Do not edit below this line
 module.exports = {
