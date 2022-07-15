13fc4d56614f9983b14bfcd6918d6de89818a27e
Amber-Graham <ang190045@gmail.com>
2022-07-15 15:42:18 -0500

    test all conditions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(date) {
+    if(date % 100 == 0 && date % 400 == 0){
+        return true;
+    }
+    if(date % 100 == 0) {
+        return false;
+    }
+    if(date % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
-};
-
-// Do not edit below this line
+//Do not edit below this line
 module.exports = leapYears;
