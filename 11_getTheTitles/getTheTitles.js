6f3514fe58713bdce9413220f447ffaa348efde6
Amber-Graham <ang190045@gmail.com>
2022-07-25 11:46:07 -0500

    pass all tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
-};
+const getTheTitles = books => books.map((book) => {
+    return(book.title);
+})
+
+
+
+
+getTheTitles(books);
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
