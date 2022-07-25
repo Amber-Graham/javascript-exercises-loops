f157d8ea78b9edb45ca52c4d25d79ea21b3a8a54
Amber-Graham <ang190045@gmail.com>
2022-07-25 09:55:20 -0500

    complete tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const clean = (str) => str.toLowerCase().replace(/[\W_]/g, '');
+    cleanStr = clean(str);
+    return cleanStr.split('').reduce((match, c, i) => {
+        if (!match) {
+            return false;
+        }
+        return c === cleanStr[cleanStr.length -1 - i];
+        }, true);
+    
+}
 
-};
+palindromes('racecar');
+palindromes('racecar!');
+palindromes('Racecar!');
+palindromes('A car, a man, a maraca.');
+palindromes('Animal loots foliated detail of stool lamina.');
+palindromes('ZZZZ car, a man, a maracaz.')
 
 // Do not edit below this line
 module.exports = palindromes;
